25bdf63fd7a4034cc388d96202c638034e1f6171
huemac16 <huemac16@htl-kaindorf.at>
2018-10-05 11:52:52 +0200

    created model

---
+++ Exercise_204/src/AvModel.java
@@ -0,0 +1,21 @@
+
+import javax.swing.table.AbstractTableModel;
+
+public class AvModel extends AbstractTableModel {
+
+    @Override
+    public int getRowCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getColumnCount() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object getValueAt(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
