eded7ee22063bd48e5c04881ab018ee63d177cf8
huemac16 <huemac16@htl-kaindorf.at>
2018-10-05 12:34:38 +0200

    update csv

--- Exercise_204/src/AvModel.java
+++ Exercise_204/src/AvModel.java
@@ -1,4 +1,7 @@
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import javax.swing.table.AbstractTableModel;
 
@@ -8,6 +11,18 @@ public class AvModel extends AbstractTableModel {
 
     private static String[] colNames = {"Bezeichnung", "AK", "Inbetr.na...", "ND", "bish. ND",
         "AfA bisher", "Wert vor ...", "AfA d. J.", "BW 31.12"};
+
+    public void add(Anlage a) {
+        anlagen.add(a);
+        fireTableRowsInserted(anlagen.size() - 1, anlagen.size() - 1);
+    }
+
+    public void load(File f) {
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+
+        } catch (Exception e) {
+        }
+    }
 
     @Override
     public int getRowCount() {
---
+++ Exercise_204/src/CellRenderer.java
@@ -0,0 +1,13 @@
+
+import java.awt.Component;
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
+
+public class CellRenderer implements TableCellRenderer {
+
+    @Override
+    public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
